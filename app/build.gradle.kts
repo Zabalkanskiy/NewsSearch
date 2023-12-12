88739bba633dacf9c8fe089231c3611d81f9d47d
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-12-12 17:19:17 +0300

    add retrofit 2 and gson convertor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,15 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //dagger
     val dagger_version = "2.49"
     implementation("com.google.dagger:dagger:$dagger_version")
     kapt("com.google.dagger:dagger-compiler:$dagger_version")
+
+    //retrofit
+    val retrofit_version = "2.9.0"
+
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
 
 }
