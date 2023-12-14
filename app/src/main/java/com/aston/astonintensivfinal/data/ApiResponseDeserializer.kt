e70b2f935fa0ed5d2c2938c571eb1e8114118582
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-12-14 16:44:07 +0300

    add deserializer for parse two response json with status error and status ok

---
+++ app/src/main/java/com/aston/astonintensivfinal/data/ApiResponseDeserializer.kt
@@ -0,0 +1,31 @@
+package com.aston.astonintensivfinal.data
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import java.lang.reflect.Type
+
+class ApiResponseDeserializer : JsonDeserializer<ApiResponse> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ApiResponse {
+        val jsonObject = json.asJsonObject
+
+        return when {
+            jsonObject.has("status") && jsonObject.get("status").asString == "error" -> {
+                Gson().fromJson(json, ErrorResponce::class.java)
+            }
+            jsonObject.has("totalResults") -> {
+                Gson().fromJson(json, NewApiResponce::class.java)
+            }
+            /* Здесь может быть логика для определения и десериализации второго типа успешного ответа */
+            else -> throw JsonParseException("Unknown type of the response")
+        }
+    }
+}
+
+// Создайте экземпляр Gson с зарегистрированным десериализатором ApiResponseDeserializer
+val gson = GsonBuilder()
+    .registerTypeAdapter(ApiResponse::class.java, ApiResponseDeserializer())
+    .create()
