6a10d0fc5a03470f5493267844be94210843a77d
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-12-21 19:28:51 +0300

    add Utils to project and android system file misc.xml

---
+++ app/src/main/java/com/aston/astonintensivfinal/utils/Utils.kt
@@ -0,0 +1,9 @@
+package com.aston.astonintensivfinal.utils
+
+object Utils {
+    const val NEWSAPIKEY : String = "0dc9dbbcf5a74911ab846d7ea7d299af"
+    const val BASEURL : String = "https://newsapi.org/v2/"
+    const val HEADLINES = "HEADLINES"
+    const val SAVED = "SAVED"
+    const val SOURCED = "SOURCED"
+}
