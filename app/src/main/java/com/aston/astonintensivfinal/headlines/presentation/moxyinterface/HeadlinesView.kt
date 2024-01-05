5feae5fbdd9dd266bd5891e33ec442a544106251
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-01-05 11:48:34 +0300

    change List type hPresenterListNewsModelData in interface presenter

--- app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/moxyinterface/HeadlinesView.kt
+++ app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/moxyinterface/HeadlinesView.kt
@@ -1,13 +1,14 @@
 package com.aston.astonintensivfinal.headlines.presentation.moxyinterface
 
 import com.aston.astonintensivfinal.data.Article
+import com.aston.astonintensivfinal.headlines.presentation.moxyinterface.model.HPresenterNewsModelData
 import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndSingleStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
 interface HeadlinesView : MvpView {
     @StateStrategyType(AddToEndSingleStrategy::class)
-    fun showData(data: List<Article>)
+    fun showData(data: List<HPresenterNewsModelData>)
 
     @StateStrategyType(AddToEndSingleStrategy::class)
     fun showError(errorMessage: String)
