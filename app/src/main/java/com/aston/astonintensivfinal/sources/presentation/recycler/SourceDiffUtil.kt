47fde0c0b804a7298aec9f93b775fc0da7560147
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-01-07 15:26:47 +0300

    create recyclerView for Sources

---
+++ app/src/main/java/com/aston/astonintensivfinal/sources/presentation/recycler/SourceDiffUtil.kt
@@ -0,0 +1,15 @@
+package com.aston.astonintensivfinal.sources.presentation.recycler
+
+import androidx.recyclerview.widget.DiffUtil
+import com.aston.astonintensivfinal.data.headlinesmodel.Article
+import com.aston.astonintensivfinal.sources.presentation.viewmodel.model.SourceNewsVM
+
+object SourceDiffUtil:  DiffUtil.ItemCallback<SourceNewsVM>() {
+    override fun areItemsTheSame(oldItem: SourceNewsVM, newItem: SourceNewsVM): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: SourceNewsVM, newItem: SourceNewsVM): Boolean {
+        return oldItem.equals(newItem)
+    }
+}
---
+++ app/src/main/java/com/aston/astonintensivfinal/sources/presentation/recycler/SourceListViewHolder.kt
@@ -0,0 +1,34 @@
+package com.aston.astonintensivfinal.sources.presentation.recycler
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.aston.astonintensivfinal.R
+import com.aston.astonintensivfinal.data.headlinesmodel.Article
+import com.aston.astonintensivfinal.sources.presentation.viewmodel.model.SourceNewsVM
+
+class SourceListViewHolder(itemView: View) : ViewHolder(itemView){
+
+    val nameAgency: TextView = itemView.findViewById(R.id.source_list_item_text_view_name_news_agency)
+    val imageAgency: ImageView = itemView.findViewById(R.id.source_list_item_source_image)
+    val categoryAgency: TextView = itemView.findViewById(R.id.source_list_item_text_view_description_agency)
+
+    fun bind(model: SourceNewsVM){
+        val sourceName = model.name ?: "Unknown source"
+        nameAgency.text = sourceName
+
+        //no category need add
+        categoryAgency.text = model.category
+
+        when {
+            sourceName.contains("Unknown source")  -> imageAgency.setImageResource(R.drawable.placeholder_bage)
+            sourceName.contains("Daily Mail", ignoreCase = true) -> imageAgency.setImageResource(R.drawable.daily_maill)
+            sourceName.contains("The New York Times", ignoreCase = true) -> imageAgency.setImageResource(R.drawable.tegraph)
+            sourceName.contains("CNN", ignoreCase = true) -> imageAgency.setImageResource(R.drawable.cnn)
+            sourceName.contains("FOX NEWS", ignoreCase = true) -> imageAgency.setImageResource(R.drawable.fox)
+            sourceName.contains("Bloomberg", ignoreCase = true) -> imageAgency.setImageResource(R.drawable.bloomberg)
+            else -> imageAgency.setImageResource(R.drawable.placeholder_bage)
+        }
+    }
+}
