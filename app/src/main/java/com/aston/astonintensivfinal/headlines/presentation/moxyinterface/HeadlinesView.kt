218a5c7b1ca7d5b6c41d3f59ce380187bace85d4
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-01-11 15:19:56 +0300

    change moxy interface HeadlinesView

--- app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/moxyinterface/HeadlinesView.kt
+++ app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/moxyinterface/HeadlinesView.kt
@@ -1,15 +1,17 @@
 package com.aston.astonintensivfinal.headlines.presentation.moxyinterface
 
-import com.aston.astonintensivfinal.data.Article
-import com.aston.astonintensivfinal.headlines.presentation.moxyinterface.model.HPresenterNewsModelData
+import com.aston.astonintensivfinal.headlines.presentation.presenter.presenterModel.headlinesNews.HPresenterNewsModelData
 import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndSingleStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
 interface HeadlinesView : MvpView {
     @StateStrategyType(AddToEndSingleStrategy::class)
-    fun showData(data: List<HPresenterNewsModelData>)
+    fun showData( totalResults: Int?, data: List<HPresenterNewsModelData>)
 
     @StateStrategyType(AddToEndSingleStrategy::class)
     fun showError(errorMessage: String)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun changeTab()
 }
