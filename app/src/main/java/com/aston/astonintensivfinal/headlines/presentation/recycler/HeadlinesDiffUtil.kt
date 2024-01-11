92f2bb9dcba4f00a2ad83ebc341dd7d860c0ad0c
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-01-11 15:16:14 +0300

    create DiffUtil and HeadlinesGeneralViewHolder refactor HeadlinesGeneralAdapter

---
+++ app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/recycler/HeadlinesDiffUtil.kt
@@ -0,0 +1,17 @@
+package com.aston.astonintensivfinal.headlines.presentation.recycler
+
+import androidx.recyclerview.widget.DiffUtil
+import com.aston.astonintensivfinal.headlines.presentation.presenter.presenterModel.headlinesNews.HPresenterNewsModelData
+
+object HeadlinesDiffUtil: DiffUtil.ItemCallback<HPresenterNewsModelData>() {
+    override fun areItemsTheSame(oldItem: HPresenterNewsModelData, newItem: HPresenterNewsModelData): Boolean {
+        //maybe wrong
+        return oldItem.url == newItem.url
+
+    }
+
+    override fun areContentsTheSame(oldItem: HPresenterNewsModelData, newItem: HPresenterNewsModelData): Boolean {
+        return oldItem.equals(newItem)
+    }
+}
+
--- app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/recycler/HeadlinesGeneralAdapter.kt
+++ app/src/main/java/com/aston/astonintensivfinal/headlines/presentation/recycler/HeadlinesGeneralAdapter.kt
@@ -1,12 +1,14 @@
-package com.aston.astonintensivfinal.headlines.recycler
+package com.aston.astonintensivfinal.headlines.presentation.recycler
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.aston.astonintensivfinal.R
-import com.aston.astonintensivfinal.data.Article
+import com.aston.astonintensivfinal.headlines.presentation.presenter.presenterModel.headlinesNews.HPresenterNewsModelData
 
-class HeadlinesGeneralAdapter(private val onClickAction: (article: Article, position : Int) -> Unit) : ListAdapter<Article, HeadlinesGeneralViewHolder>(HeadlinesDiffUtil){
+class HeadlinesGeneralAdapter(private val onClickAction: (article: HPresenterNewsModelData, position : Int) -> Unit) : ListAdapter<HPresenterNewsModelData, HeadlinesGeneralViewHolder>(
+    HeadlinesDiffUtil
+){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeadlinesGeneralViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.headlines_recycler_view_item, parent, false)
