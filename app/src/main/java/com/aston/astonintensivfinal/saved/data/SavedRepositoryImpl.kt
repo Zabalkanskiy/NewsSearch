f42f88c4821c8cc9e52055d109975df5ea028c82
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-01-14 15:55:25 +0300

    change repository

--- app/src/main/java/com/aston/astonintensivfinal/saved/data/SavedRepositoryImpl.kt
+++ app/src/main/java/com/aston/astonintensivfinal/saved/data/SavedRepositoryImpl.kt
@@ -3,6 +3,9 @@ package com.aston.astonintensivfinal.saved.data
 import com.aston.astonintensivfinal.data.databaseNews.NewsModel.NewsDao
 import com.aston.astonintensivfinal.data.databaseNews.NewsModel.NewsModelEntity
 import com.aston.astonintensivfinal.saved.domain.model.SavedNewsModelDomain
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 
 class SavedRepositoryImpl @Inject constructor(private val databaseNewsDao: NewsDao) :
@@ -13,6 +16,10 @@ class SavedRepositoryImpl @Inject constructor(private val databaseNewsDao: NewsD
 
     override fun mapNewsFromDatabaseInModel(listDBNews: List<NewsModelEntity>): List<SavedNewsModelDomain>{
 
-       return listDBNews.map { SavedNewsModelDomain(urlToImage = it.urlToImage, titleNews = it.title, content = it.content, publishedAt = it.publishedAt, source = it.sourceName) }
+        fun dateToTimestamp(date: Date): String {
+            val format = SimpleDateFormat("MMM dd, yyyy | hh:mm a", Locale.US)
+            return date.let { format.format(it) }
+        }
+       return listDBNews.map { SavedNewsModelDomain(urlToImage = it.urlToImage, titleNews = it.title, content = it.content, publishedAt = dateToTimestamp(it.publishedAt), source = it.sourceName, description = it.description, url = it.url) }
     }
 }
