e4f0e73e376fed29f1bdc6520f553bf3f1b9fcbc
Zabalkanskiy <azabalkanskij@hotmail.com>
2024-02-15 14:16:23 +0300

    BugFix error when application Died and worker need ref on application

--- app/src/main/java/com/search/news/core/DBworker/DataBaseWorker.kt
+++ app/src/main/java/com/search/news/core/DBworker/DataBaseWorker.kt
@@ -1,30 +1,34 @@
 package com.search.news.core.DBworker
 
 import android.content.Context
+import androidx.room.Room
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.search.news.SearchApplication
+import com.search.news.core.data.databaseNews.DatabaseNews
+import com.search.news.core.data.databaseNews.NewsModel.NewsDao
 import java.util.Calendar
 
-class DataBaseWorker(appContext: Context, workerParams: WorkerParameters):
+class DataBaseWorker(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
     override fun doWork(): Result {
         try {
 
 
-            val newsDao = SearchApplication.getAstonApplicationContext.newsDao
-
+            val newsDao: NewsDao =
+                Room.databaseBuilder(applicationContext, DatabaseNews::class.java, "news_db")
+                    .fallbackToDestructiveMigration().build().getNewsModelDao()
             val calendar = Calendar.getInstance()
 
-             calendar.add(Calendar.DAY_OF_YEAR, -14)
+            calendar.add(Calendar.DAY_OF_YEAR, -14)
 
             val twoWeekAgo = calendar.time
 
             newsDao.deleteNewsOlderThanTwoWeeks(twoWeekAgo)
-           return Result.success()
+            return Result.success()
         } catch (e: Exception) {
 
-           return Result.retry()
+            return Result.retry()
         }
 
 
